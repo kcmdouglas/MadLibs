cabbef50e197ac908132976bfda93f45f26eadce
Jeffrey Ruder and Kassidy Douglas <ruderjt@gmail.com>
2016-03-15 16:53:34 -0700

    Prevent loss of input caused by view recycling

--- app/src/main/java/com/epicodus/madlibs/UserInputAdapter.java
+++ app/src/main/java/com/epicodus/madlibs/UserInputAdapter.java
@@ -1,9 +1,6 @@
 package com.epicodus.madlibs;
 
 import android.content.Context;
-import android.text.Editable;
-import android.text.TextWatcher;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -12,14 +9,24 @@ import android.widget.EditText;
 
 import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.Map;
 
 public class UserInputAdapter extends ArrayAdapter<String> {
-    HashMap<Integer, String> positionsValues = new HashMap<Integer, String>();
-
     public UserInputAdapter(Context context, ArrayList<String> inputs) {
         super(context, 0, inputs);
     }
+
+    //Hack - don't do this normally
+    //Disabling view recycling defeats the purpose of using a ListView - JTR
+    @Override
+    public int getViewTypeCount() {
+        return getCount();
+    }
+
+    @Override
+    public int getItemViewType(int position) {
+        return position;
+    }
+
 
     @Override
     public View getView(final int position, View convertView, ViewGroup parent) {
@@ -34,34 +41,7 @@ public class UserInputAdapter extends ArrayAdapter<String> {
         EditText etInput = (EditText) convertView.findViewById(R.id.user_input);
         // Populate the data into the template view using the data object
         etInput.setHint(input);
-
-        String oldText = positionsValues.get(position);
-        if (oldText != null) {
-            Log.d("old", position + "");
-            Log.d("old", oldText);
-        }
-
-        if (oldText == null) {
-            etInput.setText("");
-        } else {
-            etInput.setText(oldText);
-        }
-
-        etInput.addTextChangedListener(new TextWatcher() {
-            @Override
-            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
-
-            }
-
-            @Override
-            public void onTextChanged(CharSequence s, int start, int before, int count) {
-            }
-
-            @Override
-            public void afterTextChanged(Editable s) {
-                positionsValues.put(position, s.toString());
-            }
-        });
+//
         // Return the completed view to render on screen
         return convertView;
     }
